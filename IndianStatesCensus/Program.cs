60df4684608e68094b343a0472bfae6aad8e3f56
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-27 09:25:49 +0530

    [Tirupathi] Add. Welcome message to master

---
+++ IndianStatesCensus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census program");
+        }
+    }
+}
