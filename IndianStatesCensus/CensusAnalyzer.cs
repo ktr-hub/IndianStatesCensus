6a5a242bfa00e81971be1492b3f3b9ac926b744e
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-29 06:52:26 +0530

    [Tirupathi] Add. Design principles to census analyzer

---
+++ IndianStatesCensus/CensusAnalyzer.cs
@@ -0,0 +1,55 @@
+﻿using IndianStatesCensus.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.XPath;
+
+namespace IndianStatesCensus
+{
+    public class CensusAnalyzer
+    {
+        public enum Country
+        {
+            INDIA,US,BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string,CensusDTO> LoadCensusData(Country country,string csvFilePath,string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+        //public object GetSortedStateCodeDataInJsonFormat(Country country,string csvFilePath,string dataheaders,string sortField, SortOrder.S)
+        //{
+        //    var censusData = LoadCensusData(country, csvFilePath, dataheaders);
+        //    List<CensusDTO> lines = censusData.Values.ToList();
+        //    List<CensusDTO> lists = GetSortedData(sortField, lines);
+        //    if (sortBy.Equals(SortOrder.SortBy.DESC))
+        //    {
+        //        lists.Reverse();
+        //    }
+        //    return JsonConvert.SerializeObject(lists);
+        //}
+
+        //public List<CensusDTO> GetSortedData(string sortField,List<CensusDTO> lines)
+        //{
+        //    switch (sortField)
+        //    {
+        //        case "stateName": return lines.OrderBy(x => x.stateName).ToList();
+        //        case "stateCode": return lines.OrderBy(x => x.stateCode).ToList();
+        //        case "state": return lines.OrderBy(x => x.state).ToList();
+        //        case "area": return lines.OrderBy(x => x.area).ToList();
+        //        case "usArea": return lines.OrderBy(x => x.totalArea).ToList();
+        //        case "populationDensity": return lines.OrderBy(x => x.populationDensity).ToList();
+        //        case "density": return lines.OrderBy(x => x.density).ToList();
+        //        case "population": return lines.OrderBy(x=>x.population).ToList();
+        //        default: return lines.OrderBy(x => x.tin).ToList();
+        //    }
+        //}
+
+    }
+}
---
+++ NUnitTestProject1/CensusAnalyserTest.cs
@@ -0,0 +1,77 @@
+using NUnit.Framework;
+using IndianStatesCensus.DTO;
+using IndianStatesCensus.POCO;
+using static IndianStatesCensus.CensusAnalyzer;
+using System.Collections.Generic;
+using IndianStatesCensus;
+
+namespace NUnitTestProject1
+{
+    public class CensusAnalyserTest
+    {
+        static string indianStateCensusHeaders = @"State,Population,AreaInSqKm,DensityPerSqKm";
+        static string indianStateCodeHeaders = @"SrNo,State Name,TIN,StateCode";
+        static string indianStateCensusFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\IndiaStateCensusData.csv";
+        static string wrongHeaderIndianCensusFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\WrongIndiaStateCensusData.csv";
+        static string delimeterIndianCensusFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\DelimiterIndiaStateCensusData.csv";
+        static string wrongIndianStateCensusFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\WrongIndiaStateCensusData.csv";
+        static string wrongIndianCensusFileType = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\IndiaStateCensusData.txt";
+        static string indianStateCodeFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\IndiaStateCode.csv";
+        static string wrongIndianStateCodeFileType = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\IndiaStateCode.txt";
+        static string delimeterIndianStateCodeFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\DelimiterIndiaStateCode.csv";
+        static string wrongHeaderStateCodeFilePath = @"C:\Users\Tirupathi Rao\source\repos\IndianStatesCensus\IndianStatesCensus\CSVFiles\WrongIndiaStateCode.csv";
+
+        IndianStatesCensus.CensusAnalyzer censusAnalyzer;
+        Dictionary<string, CensusDTO> totalRecord;
+        Dictionary<string, CensusDTO> stateRecord;
+
+        [SetUp]
+        public void Setup()
+        {
+            censusAnalyzer = new CensusAnalyzer();
+            totalRecord = new Dictionary<string, CensusDTO>();
+            stateRecord = new Dictionary<string, CensusDTO>();
+        }
+
+        //HAPPY Test Case
+        [Test]
+        public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
+        {
+            totalRecord = censusAnalyzer.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(29,totalRecord.Count);
+        }
+
+        //SAD Test Case
+        [Test]
+        public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnException()
+        {
+            totalRecord = censusAnalyzer.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(37, totalRecord.Count);
+        }
+
+        //SAD Test Case
+        [Test]
+        public void GivenIndianCensusDataFileTypeWrong_WhenReaded_ShouldReturnException()
+        {
+            totalRecord = censusAnalyzer.LoadCensusData(Country.INDIA, wrongIndianCensusFileType, indianStateCensusHeaders);
+            Assert.AreEqual(29, totalRecord.Count);
+        }
+
+        //SAD Test Case
+        [Test]
+        public void GivenIndianCensusDataFileDelimeterWrong_WhenReaded_ShouldReturnException()
+        {
+            totalRecord = censusAnalyzer.LoadCensusData(Country.INDIA, delimeterIndianCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(29, totalRecord.Count);
+        }
+
+        //SAD Test Case
+        [Test]
+        public void GivenIndianCensusDataFileCsvHeaderWrong_WhenReaded_ShouldReturnException()
+        {
+            totalRecord = censusAnalyzer.LoadCensusData(Country.INDIA, wrongHeaderIndianCensusFilePath, wrongHeaderIndianCensusFilePath);
+            Assert.AreEqual(29, totalRecord.Count);
+        }
+
+    }
+}
